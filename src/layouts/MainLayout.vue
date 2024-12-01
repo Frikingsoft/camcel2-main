21cd7c825a2b4e9b6a46900ad3959040c7c2248b
Mauro Caetano <lucianocaetano1999@gmail.com>
2024-12-01 17:22:05 -0300

    ultimos cambios

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,22 +2,18 @@
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
       <MenuToolbarLayout>
-        <router-view @click="ver_menu =false"/>
+        <router-view @click="ver_menu = false" />
       </MenuToolbarLayout>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup>
-import { provide, ref } from 'vue'
-import MenuToolbarLayout from "./MenuToolbarLayout.vue"
-const ver_menu = ref(false)
-provide("ver_menu",ver_menu)
+import { provide, ref } from "vue";
+import MenuToolbarLayout from "./MenuToolbarLayout.vue";
+const ver_menu = ref(false);
+provide("ver_menu", ver_menu);
 defineOptions({
-  name: 'MainLayout'
-})
-
-
-
-
+  name: "MainLayout",
+});
 </script>
